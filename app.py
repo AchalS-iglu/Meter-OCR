dbf6b762992e2c406b936fda888b18a260d7ebb9
Achal Singhal <achals2002@gmail.com>
2021-12-09 03:06:34 +0530

    Tesseract OCR implemented

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ import os
 import cv2
 import numpy as np
 import matplotlib.pyplot as plt
+import pytesseract
 
 from modules.jpgtohsv import jpgtohsv
 
@@ -10,6 +11,9 @@ path = "./images/"
 files = [file for file in os.listdir(path) if file.endswith('.jpg')]
 #print(sorted(files))
 
+custom_config = r'--oem 3 --psm 6'
+pytesseract.pytesseract.tesseract_cmd = "C:/Program Files/Tesseract-OCR/tesseract"
+TESSDATA_PREFIX = 'C:/Program Files/Tesseract-OCR'
 
 while True:
     for image in files:
@@ -28,8 +32,12 @@ while True:
         
         cv2.drawContours(img, contours, -1, (0, 255, 0), 3)
         cv2.imshow('Contours', img)
+        
+        reading = pytesseract.image_to_string(img_edge, config=custom_config)
+        print(reading)
     
         key = cv2.waitKey(30000)
         if key == ord('q') or key == 27:
             continue
+
     break
